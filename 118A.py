39ab5a0a2ae380a69fa95d89c94a395f8df2f5b3
Rahul Podugu <38282313+Rahul2251999@users.noreply.github.com>
2018-12-07 13:34:45 +0530

    Update 118A.py

--- 118A.py
+++ 118A.py
@@ -2,7 +2,7 @@ k=input()
 k=k.lower()
 p=[]
 for i in range(len(k)):
-    if k[i]=='a' or k[i]=='e' or k[i]=='i' or k[i]=='o' or k[i]=='u' or k[i]=='y':
+    if k[i]=='a' or k[i]=='e' or k[i]=='i' or k[i]=='o' or k[i]=='u' or k[i]=='y': #ffgdhtgh
         True
     else:
         p.append('.'+k[i])
