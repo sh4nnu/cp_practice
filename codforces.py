6ffb872b3052c978728a897a8646331b1874c0cb
Rahul Podugu <38282313+Rahul2251999@users.noreply.github.com>
2019-10-01 23:45:20 +0530

    Update codforces.py

--- codforces.py
+++ codforces.py
@@ -6,5 +6,5 @@ min = l[0] / l[1]
 for i in range(n[0]-1):
     l = list(map(int, input().split()))
     if (min >= l[0] / l[1]):
-        min = l[0] / l[1]
+        min = l[0] / l[1]       #sgdffffffffffffffffffff
 print(m*min)
